ad4f045f2961ee6d415fa0c902aa25daf475d8be
jj <jjyeong2103@snu.ac.kr>
2023-05-31 10:48:56 +0900

    new50

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ st.markdown("저장 매체의 용량을 나타내는 단위로 B, KB, MB 등이 
 response = st.text_input('답안 :', key='answer_input_2')
 
 #모델의 이름 정하기
-model_name = "1-8_rnn_sp_140" #모델 이름 넣어주기 확장자는 넣지말기!
+model_name_1_8 = "1-8_rnn_sp_140" #모델 이름 넣어주기 확장자는 넣지말기!
 #모델에 맞는 hyperparameter 설정
 vs = 140 #vocab size
 emb = 16 #default 값 지정 안했으면 건드리지 않아도 됨
@@ -95,15 +95,15 @@ max_len = 100
 output_d = 6 #자기의 모델에 맞는 output_d구하기 (지식요소 개수)
 c = cfg(vs=vs, emb=emb, hidden=hidden, nh=nh, device=device)
 
-model = RNNModel(output_d, c) #RNNModel 쓰는경우
+model_1_8 = RNNModel(output_d, c) #RNNModel 쓰는경우
 # model = LSTMModel(output_d, c) #LSTMModel 쓰는경우
 # model = ATTModel(output_d, c) #ATTModel 쓰는경우
 
-model.load_state_dict(torch.load("./save/"+model_name+".pt"))
+model_1_8.load_state_dict(torch.load("./save/"+model_name_1_8+".pt"))
 
 #자신에게 맞는 모델로 부르기
-tokenizer = AutoTokenizer.from_pretrained("./save/"+model_name) #sp tokenizer 쓰는 경우
-# tokenizer = BertTokenizer.from_pretrained("./save/"+model_name+"-vocab.txt") #bw tokenizer 쓰는경우
+tokenizer = AutoTokenizer.from_pretrained("./save/"+model_name_1_8) #sp tokenizer 쓰는 경우
+# tokenizer = BertTokenizer.from_pretrained("./save/"+model_name_1_8+"-vocab.txt") #bw tokenizer 쓰는경우
 
 enc = tokenizer(response)["input_ids"] #sp tokenizer
 # enc = tokenizer.encode(response) #bw tokenizer
@@ -113,7 +113,7 @@ if l < max_len :
 else : pad = enc[l-max_len:]
 pad_ten = torch.tensor(pad)
 pad_ten = pad_ten.reshape(1,max_len)
-y = model(pad_ten)
+y = model_1_8(pad_ten)
 label = y.squeeze().detach().cpu().numpy().round()
 
 if st.button('피드백 받기'):
