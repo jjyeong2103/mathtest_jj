b31567e68318cae7adf42d4f15984421231eb84f
jj <jjyeong2103@snu.ac.kr>
2023-05-31 17:12:10 +0900

    ver46

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 st.title("자동 채점 모델 기반 자동 피드백")
 
 st.markdown("---")
-st.write("**팀원** : 수학교육과 김명식, 김재훈, 김지영, 신인섭, 윤예린, 정유진")
+st.write("**팀원** : ✨수학교육과 김명식, 김재훈, 김지영, 신인섭, 윤예린, 정유진✨")
 st.markdown("---")
 
 import streamlit as st
