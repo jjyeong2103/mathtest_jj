1bd9a20e4f0763176c428a508587032fd3a93391
jj <jjyeong2103@snu.ac.kr>
2023-05-31 14:46:26 +0900

    new106

--- app.py
+++ app.py
@@ -50,13 +50,13 @@ else:
 pad_ten = torch.tensor(pad)
 pad_ten = pad_ten.reshape(1, max_len)
 y = model_1_1(pad_ten)
-label_1_1 = y.squeeze().detach().cpu().numpy().round()
+label = y.squeeze().detach().cpu().numpy().round()
 
 if st.button('피드백 받기', key='button1_1_1_1'):
     
     # output차원에 맞추어 피드백 넣기
  st.write(response)
- if label_1_1[0] == 1:
+ if label[0] == 1:
      st.success('거듭제곱의 곱셈을 이해하고 있구나!', icon="✅")
  else:
      st.info('거듭제곱의 곱셈을 복습하세요!', icon="⚠️")
