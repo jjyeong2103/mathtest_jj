a39e572238d1c4c3bbb213e4fc5a8d59169019a1
jj <jjyeong2103@snu.ac.kr>
2023-05-31 12:55:41 +0900

    new65

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ if st.button('피드백 받기', key='button_1_8_1'):
     #output차원에 맞추어 피드백 넣기
     st.write(response)
     if len(label) >= 5:
-        if label[0] == 1 and label[1] == 1 and label[2] == 1:
+        if label[0] == 1 and label[1] == 1 and label[2] == 1 and label[3] == 0 and label[4] == 0:
             st.success('거듭제곱의 곱셈, 거듭제곱의 나눗셈, 단위 변환을 이해하고 있구나!', icon="✅")
         elif label[0] == 1 and label[2] == 0 and label[2] == 1:
             st.success('거듭제곱의 거듭제곱, 단위 변환을 이해하고 있구나! 거듭제곱의 나눗셈을 올바르게 적용해서 풀이를 완성해보자!', icon="ℹ️")
