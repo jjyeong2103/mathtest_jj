1c503a11dd47c0430567b54ef265a00b1660f3f7
jj <jjyeong2103@snu.ac.kr>
2023-05-31 17:20:43 +0900

    ver51

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ st.markdown("---")
 import streamlit as st
 import time
 with st.spinner(text='오늘 수업 즐겁게 들었나요? 이제 여러분들이 얼마나 공부를 열심히 했는지 알아보도록 해요!'):
-    time.sleep(4)
+    time.sleep(2)
     st.success('자, 시작해볼까요?')
 
-from streamlit_awesome_icons import icon
+from streamlit_awesome_icons import icon_name
 
-st.markdown(icon("fas fa-user"))
+st.markdown(icon_name("fas fa-user"))
 
 # 문항1-1
 
