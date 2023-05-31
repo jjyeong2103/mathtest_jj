8ea2eac71d0de872df73ba5c8f59ffb18c188a21
jj <jjyeong2103@snu.ac.kr>
2023-05-31 10:54:46 +0900

    modelNameModified

--- app.py
+++ app.py
@@ -75,6 +75,16 @@ if st.button('힌트 보기'):
  
 
 st.markdown("---")
+
+
+
+
+
+
+
+
+
+
 #문항1-8
 
 st.subheader("문항1-8")
@@ -92,20 +102,20 @@ nh = 4 #default 값 지정 안했으면 건드리지 않아도 됨
 device = "cpu" #default 값 지정 안했으면 건드리지 않아도 됨
 max_len = 100
 #output_d 설정
-output_d = 6 #자기의 모델에 맞는 output_d구하기 (지식요소 개수)
+output_d_6 = 6 #자기의 모델에 맞는 output_d구하기 (지식요소 개수)
 c = cfg(vs=vs, emb=emb, hidden=hidden, nh=nh, device=device)
 
-model_1_8 = RNNModel(output_d, c) #RNNModel 쓰는경우
+model_1_8 = RNNModel(output_d_6, c) #RNNModel 쓰는경우
 # model = LSTMModel(output_d, c) #LSTMModel 쓰는경우
 # model = ATTModel(output_d, c) #ATTModel 쓰는경우
 
 model_1_8.load_state_dict(torch.load("./save/"+model_name_1_8+".pt"))
 
 #자신에게 맞는 모델로 부르기
-tokenizer = AutoTokenizer.from_pretrained("./save/"+model_name_1_8) #sp tokenizer 쓰는 경우
+tokenizer_1_8 = AutoTokenizer.from_pretrained("./save/"+model_name_1_8) #sp tokenizer 쓰는 경우
 # tokenizer = BertTokenizer.from_pretrained("./save/"+model_name_1_8+"-vocab.txt") #bw tokenizer 쓰는경우
 
-enc = tokenizer(response)["input_ids"] #sp tokenizer
+enc = tokenizer_1_8(response)["input_ids"] #sp tokenizer
 # enc = tokenizer.encode(response) #bw tokenizer
 l = len(enc)
 if l < max_len :
