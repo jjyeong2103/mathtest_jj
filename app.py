c97f962eb312d25d414a2efae8533e41ca4d74c7
jj <jjyeong2103@snu.ac.kr>
2023-05-31 14:36:07 +0900

    new99

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ y = model_1_1(pad_ten)
 label_1_1 = y.squeeze().detach().cpu().numpy().round()
 
 if st.button('피드백 받기', key='button1_1_1_1'):
+    
     # output차원에 맞추어 피드백 넣기
     st.write(response)
     if label_1_1 is not None and len(label_1_1) >= 1:
@@ -60,8 +61,8 @@ if st.button('피드백 받기', key='button1_1_1_1'):
             st.success('거듭제곱의 곱셈을 이해하고 있구나!', icon="✅")
         else:
             st.info('거듭제곱의 곱셈을 복습하세요!', icon="⚠️")
-else:
-    st.info('거듭제곱의 곱셈을 복습하세요!', icon="⚠️")
+#else:
+    #st.info('거듭제곱의 곱셈을 복습하세요!', icon="⚠️")
 
 
 if st.button('힌트 보기', key='button1_1_1_2'):
