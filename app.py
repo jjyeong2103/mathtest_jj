91f9aee9268013cdbfd0cb690c5eab5e1c079b8b
jj <jjyeong2103@snu.ac.kr>
2023-06-05 12:09:52 +0900

    group46

--- app.py
+++ app.py
@@ -530,7 +530,7 @@ if st.button('❓힌트 보기', key='button_1_8_2'):
 import pandas as pd
 ###1-7
 st.header("1-7")
-st.write("**문제** : $(2^4)^x\times(2^3)^x=2^3\times2^{3x}$")
+st.write("**문제** : $ (2^4)^x \\times (2^3)^x = 2^3 \\times 2^{3x} $")
 st.write("**지식요소** : 거듭제곱의 거듭제곱, 거듭제곱의 곱셈, 일차방정식의 풀이")
 #latex_equation1 = r"""
 
